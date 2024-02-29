94d3ac0b52acc7dec0c83c8b1d6f93dacca94313
ParkDongJo <pdj10211@gmail.com>
2024-03-01 05:40:34 +0900

    Style: 일부 파일삭제, 가벼운 수정

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 프론트단에서 해볼 수 있는 실험 및 연습 모음
+
+https://www.reacterry.com/portal/challenges
--- practice-react-components/src/App.js
+++ practice-react-components/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
-import Carousel from "./Carousel";
-import SubmitMessage from "./SubmitMessage";
-import SelectAll from "./SelectAll";
-import DebounceTest from "./DebounceTest";
+import Carousel from "./components/Carousel";
+import SubmitMessage from "./components/SubmitMessage";
+import SelectAll from "./components/SelectAll";
+import DebounceTest from "./components/DebounceTest";
+import InfinityScroll from "./components/InfinityScroll";
+import InfinityScroll2 from "./components/InfinityScroll2";
 
 function App() {
   return (
     <div className="App">
-      <DebounceTest />
+      <InfinityScroll2 />
     </div>
   );
 }
