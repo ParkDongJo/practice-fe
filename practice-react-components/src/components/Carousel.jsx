9065b53dbd2b07388f400b9bb56c8d7fa8140102
ParkDongJo <pdj10211@gmail.com>
2024-03-01 05:39:24 +0900

    Refactor: 컴포넌트 파일 /components 구조로 옮기기

---
+++ practice-react-components/src/components/Carousel.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+export default function Carousel() {
+  const images = [
+    { id: 1, name: "image1", url: "" },
+    { id: 2, name: "image2", url: "" },
+    { id: 3, name: "image3", url: "" },
+  ];
+  const [curr, setCurr] = useState(0);
+
+  const handleClickPrev = () => {
+    setCurr((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
+  const handleClickNext = () => {
+    setCurr((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+
+  return (
+    <div className="relative flex h-[500px] items-center overflow-hidden bg-black-100">
+      <ul
+        className={`flex h-[300px] items-center ${moveStyle[curr]} transition`}
+      >
+        {images.map((image) => (
+          <li key={image.id} className="w-[100vw] h-[300px]">
+            <img
+              src={image.url}
+              alt={image.name}
+              className="object-contain w-full"
+            />
+          </li>
+        ))}
+      </ul>
+      <button
+        className="absolute left-[10px] flex h-[10px] w-[10px] items-center justify-center rounded-full bg-black-10 opacity-50"
+        onClick={handleClickPrev}
+      >
+        이전
+      </button>
+      <button
+        className="absolute right-[10px] flex h-[10px] w-[10px] items-center justify-center rounded-full bg-black-10 opacity-50"
+        onClick={handleClickNext}
+      >
+        다음
+      </button>
+      <ul className="absolute flex justify-center w-full gap-4 bottom-20">
+        {images.map((image, idx) => (
+          <li
+            key={image.id}
+            className={`h-[1.2rem] w-[1.2rem] rounded-full bg-white ${
+              idx === curr ? "opacity-100" : "opacity-50"
+            }`}
+          >
+            <button onClick={() => setCurr(idx)}>o</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+const moveStyle = {
+  0: "translate-x-0",
+  1: "translate-x-[-100vw]",
+  2: "translate-x-[-200vw]",
+  3: "translate-x-[-300vw]",
+  4: "translate-x-[-400vw]",
+  5: "translate-x-[-500vw]",
+  6: "translate-x-[-600vw]",
+  7: "translate-x-[-700vw]",
+  8: "translate-x-[-800vw]",
+  9: "translate-x-[-900vw]",
+  10: "translate-x-[-1000vw]",
+};
---
+++ practice-react-components/src/components/DebounceTest.jsx
@@ -0,0 +1,33 @@
+import { useState, useMemo } from "react"
+import debouce from "../libs/debouce"
+
+const promiseFun = async (value) => {
+    await new Promise((resolve) => setTimeout(resolve, 300));
+    return "abcd" === value;
+};
+
+export default function DebounceTest() {
+    const [check, setCheck] = useState(false);
+    const [value, setValue] = useState('');
+    
+    const handleChange = async (e) => {
+        const result = await promiseFun(e.target.value);
+        setValue(e.target.value);
+        setCheck(result);
+    };
+    
+    const debouncedOnChange = useMemo(
+        () => debouce(handleChange, 300)
+    ,[])
+
+    return (
+        <div>
+            <h2>중복 문자열 "abcd"</h2>
+            <h3>check : {check ? "true" : "false"}</h3>
+            <h3>Debounced value: {value}</h3>
+            <input type="text" onChange={debouncedOnChange} />
+            <hr />
+            <button disabled={check}>BUTTON</button>
+        </div>
+    );
+}
