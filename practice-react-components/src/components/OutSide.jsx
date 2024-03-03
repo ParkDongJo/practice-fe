c08cece01c24c680efecada768dda8e3b45beaf5
ParkDongJo <pdj10211@gmail.com>
2024-03-03 18:17:18 +0900

    Feat: ClickOutSide 예제 구현

--- practice-react-components/src/components/OutSide.jsx
+++ practice-react-components/src/components/OutSide.jsx
@@ -1,6 +1,43 @@
+import { useRef, useState } from "react";
+import styled from "styled-components";
+import useClickOutside from "../hooks/useClickOutSide";
+
 export default function OutSide() {
-  return (
-    <div>
-    </div>
-  )
+    const innerRef = useRef();
+    const [isOpen, setIsOpen] = useState(false);
+
+    useClickOutside(innerRef, () => {
+        setIsOpen(false);
+    }, [innerRef]);
+
+    return (
+        <Outter>
+            <h1>OutSide</h1>
+            <InnerContainer ref={innerRef}>
+                <button onClick={() => setIsOpen(!isOpen)}>Toggle</button>
+                {isOpen && (<Inner>
+                    Click outside of this box
+                </Inner>)}
+            </InnerContainer>
+        </Outter>
+    )
 }
+
+const Outter = styled.div`
+    width: 100%;
+    height: 600px;
+    padding: 20px;
+    border: 1px solid black;
+`;
+const InnerContainer = styled.div`
+    width: 100%;
+    height: 400px;
+    padding: 20px;
+    border: 1px solid blue;
+`;
+const Inner = styled.div`
+    width: 300px;
+    height: 300px;
+    padding: 20px;
+    border: 1px solid red;
+`;
--- practice-react-components/src/hooks/useClickOutSide.js
+++ practice-react-components/src/hooks/useClickOutSide.js
@@ -1,16 +1,17 @@
 import { useEffect } from 'react';
 
 export default function useClickOutside(ref, callback) {
-    const handleClick = e => {
-      if (ref.current && !ref.current.contains(e.target)) {
-        callback();
-      }
-    };
-
     useEffect(() => {
-      document.addEventListener('click', handleClick);
-      return () => {
-        document.removeEventListener('click', handleClick);
-      };
-    });
+        const handleClick = e => {
+            if (ref.current && !ref.current.contains(e.target)) {
+                console.log('click outside');
+                callback();
+            }
+        };
+
+        document.addEventListener('click', handleClick);
+        return () => {
+            document.removeEventListener('click', handleClick);
+        };
+    }, [ref, callback]);
 };
